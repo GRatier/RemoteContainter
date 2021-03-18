b90d5d5b0a65e97187b640669cf33d3dfbc704ce
Game <gameupdate.paypal@gmail.com>
2021-03-18 17:40:02 +0100

    edit

--- src/test/java/CucumberTest.java
+++ src/test/java/CucumberTest.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "src/main/resources/")
+		features = "src/main/resources/Client_login.feature")
 
 //		features = "src/main/resources/ClientProvideAccess.feature")
 //		features = "src/main/resources/Client_login.feature")
