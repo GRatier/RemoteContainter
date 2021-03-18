4d522c2633abda5385ff497f3423be4b03259769
Game <gameupdate.paypal@gmail.com>
2021-03-18 17:36:29 +0100

    fixes

--- src/test/java/runners/CucumberTest.java
+++ src/test/java/runners/CucumberTest.java
@@ -1,10 +1,11 @@
+package runners;
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "Ctests/Csteps/")
+		features = "src/test/java/features/")
 
 //		features = "src/main/resources/ClientProvideAccess.feature")
 //		features = "src/main/resources/Client_login.feature")
--- src/test/java/steps/LoginLogoutSteps.java
+++ src/test/java/steps/LoginLogoutSteps.java
@@ -1,4 +1,4 @@
-package javaSteps;
+package steps;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
