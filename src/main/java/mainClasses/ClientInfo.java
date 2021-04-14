f5403d039631dfc7d0b580a00fe30306aaa0e0b0
Game <gameupdate.paypal@gmail.com>
2021-04-14 14:11:23 +0200

    Adds

--- src/main/java/mainClasses/ClientInfo.java
+++ src/main/java/mainClasses/ClientInfo.java
@@ -1,13 +1,61 @@
 package mainClasses;
 
 import domains.Address;
+import domains.Client;
 
 public class ClientInfo {
 	
 	private String name;
 	private String email;
-	private Address address;
 	private String reference;
-	
+	private Address address;
 
+	public ClientInfo(String name, String email, String reference_person) {
+		this.name = name;
+		this.email = email;
+		this.reference = reference_person;
+	}
+	
+	public ClientInfo(Client client) {
+		this.name = client.getName();
+		this.email = client.getEmail();
+		this.reference = client.getRefPerson();
+		this.address = client.getAddress();
+	}
+	
+	public String getReference_person() {
+		return reference;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+	public String getZipCode() {
+		return address.getPostCode();
+	}
+	
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+	
+	public Client asClient() {
+		return new Client(this);
+	}
+	
+	public void setRefPerson(String refPerson) {
+		this.reference = refPerson;
+	}
+	
+	public String toString() {
+		return getName() + ", " + getEmail();
+	}
+	
 }
