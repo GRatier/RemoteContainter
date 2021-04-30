c67b3dc07f183e0f0a7c399c33250cdd83d2686f
mo3ni <mohseni.mr93@gmail.com>
2021-04-30 17:34:51 +0200

    Add files via upload

---
+++ LogisticCompany.java
@@ -0,0 +1,49 @@
+package mainClasses;
+
+import domains.LogisticCom;
+import mainClasses.ContainerInfo;
+
+//
+
+public class LogisticCompany {
+	
+	
+	public void updateAirHumidity(LogisticCom airHumidity,ContainerInfo containerId) {
+		
+		// if container Id exist then update else no
+		//LogisticCom airHum = getAirHumidity(airHumidity);
+		
+		if(ContainerInfo.contains(containerId)) {
+			return;
+		}
+		
+		else {
+			System.out.println("No Container found!");
+		}
+	}
+		
+		
+		public void updateAtmPressure(LogisticCom atmPresssure,ContainerInfo containerId) {
+			
+			if(ContainerInfo.contains(containerId)) {
+				return;
+			}
+			
+			else {
+				System.out.println("No Container found!");
+			}
+		}
+			
+		public void updateInternalTemp(LogisticCom internalTemp,ContainerInfo containerId) {
+				
+				if(ContainerInfo.contains(containerId)) {
+					return;
+				}
+				
+				else {
+					System.out.println("No Container found!");
+				}
+		}		
+		
+}
+	
